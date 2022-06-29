ed6e1a47418047d57423537234fe2b528a4a0412
lihbr <lihbr@users.noreply.github.com>
2022-06-29 13:22:16 +0200

    test: `HookSystem` class

--- packages/kit/test/lib-HookSystem.test.ts
+++ packages/kit/test/lib-HookSystem.test.ts
@@ -1,8 +1,6 @@
 import { it, expect, vi } from "vitest";
 
-import { HookSystem } from "../src/lib";
-
-// TODO: Refactor tests to clearly separate base usage from createScope usage
+import { HookError, HookSystem } from "../src/lib";
 
 it("calls hooks", async () => {
 	const system = new HookSystem();
@@ -28,36 +26,61 @@ it("calls hooks", async () => {
 	expect(baz).toHaveBeenCalledTimes(1);
 });
 
-it("calls hooks with scoped additional args", async () => {
+it("calls hooks from scope", async () => {
 	const system = new HookSystem();
 
 	const { hook: fooHook } = system.createScope("foo", ["fooArg1", "fooArg2"]);
 	const { hook: barHook } = system.createScope("bar", ["barArg1"]);
 
-	const foo = vi.fn();
-	const bar = vi.fn();
+	const baz = vi.fn();
+	const qux = vi.fn();
+	const quux = vi.fn();
 
-	fooHook("hook", foo);
-	barHook("hook", bar);
+	fooHook("hook1", baz);
+	barHook("hook1", qux);
+	barHook("hook2", quux);
+
+	await system.callHook("hook1");
+
+	expect(baz).toHaveBeenCalledTimes(1);
+	expect(qux).toHaveBeenCalledTimes(1);
+	expect(quux).not.toHaveBeenCalled();
+
+	await system.callHook("hook2");
+
+	expect(baz).toHaveBeenCalledTimes(1);
+	expect(qux).toHaveBeenCalledTimes(1);
+	expect(quux).toHaveBeenCalledTimes(1);
+});
+
+it("calls hooks from scope with additional args", async () => {
+	const system = new HookSystem();
+
+	const { hook: fooHook } = system.createScope("foo", ["fooArg1", "fooArg2"]);
+	const { hook: barHook } = system.createScope("bar", ["barArg1"]);
+
+	const baz = vi.fn();
+	const qux = vi.fn();
+
+	fooHook("hook", baz);
+	barHook("hook", qux);
 
 	await system.callHook("hook", "quxArg1");
 
-	expect(foo).toHaveBeenCalledTimes(1);
-	expect(foo).toHaveBeenCalledWith("quxArg1", "fooArg1", "fooArg2");
-	expect(bar).toHaveBeenCalledTimes(1);
-	expect(bar).toHaveBeenCalledWith("quxArg1", "barArg1");
+	expect(baz).toHaveBeenCalledTimes(1);
+	expect(baz).toHaveBeenCalledWith("quxArg1", "fooArg1", "fooArg2");
+	expect(qux).toHaveBeenCalledTimes(1);
+	expect(qux).toHaveBeenCalledWith("quxArg1", "barArg1");
 });
 
 it("stops calling hook when unhooked", async () => {
 	const system = new HookSystem();
 
-	const { hook, unhook } = system.createScope("root", []);
-
 	const foo = vi.fn();
 	const bar = vi.fn();
 
 	system.hook("root", "hook1", foo);
-	hook("hook1", bar);
+	system.hook("root", "hook1", bar);
 
 	await system.callHook("hook1");
 
@@ -71,12 +94,43 @@ it("stops calling hook when unhooked", async () => {
 	expect(foo).toHaveBeenCalledTimes(1);
 	expect(bar).toHaveBeenCalledTimes(2);
 
-	unhook("hook1", bar);
+	system.unhook("hook1", bar);
 
 	await system.callHook("hook1");
 
 	expect(foo).toHaveBeenCalledTimes(1);
 	expect(bar).toHaveBeenCalledTimes(2);
+});
+
+it("stops calling hook from scope when unhooked", async () => {
+	const system = new HookSystem();
+
+	const { hook, unhook } = system.createScope("foo", ["fooArg1", "fooArg2"]);
+
+	const bar = vi.fn();
+	const baz = vi.fn();
+
+	hook("hook1", bar);
+	hook("hook1", baz);
+
+	await system.callHook("hook1");
+
+	expect(bar).toHaveBeenCalledTimes(1);
+	expect(baz).toHaveBeenCalledTimes(1);
+
+	unhook("hook1", bar);
+
+	await system.callHook("hook1");
+
+	expect(bar).toHaveBeenCalledTimes(1);
+	expect(baz).toHaveBeenCalledTimes(2);
+
+	unhook("hook1", baz);
+
+	await system.callHook("hook1");
+
+	expect(bar).toHaveBeenCalledTimes(1);
+	expect(baz).toHaveBeenCalledTimes(2);
 });
 
 it("returns an empty array when no hook registered", async () => {
@@ -103,6 +157,34 @@ it("returns hook returned values in order", async () => {
 	expect(result).toStrictEqual({ data: ["foo", "bar"], errors: [] });
 });
 
+it("returns hook errors in order", async () => {
+	const system = new HookSystem();
+
+	const foo = vi.fn(() => {
+		throw new Error("foo");
+	});
+	const bar = vi.fn(() => {
+		throw "bar";
+	});
+
+	system.hook("baz", "hook1", foo);
+	system.hook("qux", "hook1", bar);
+
+	const result = await system.callHook("hook1");
+
+	expect(result.data.length).toBe(0);
+	expect(result.errors.length).toBe(2);
+
+	expect(result.errors[0]).toBeInstanceOf(HookError);
+	expect(result.errors[0].owner).toBe("baz");
+	expect(result.errors[0].cause).toBeInstanceOf(Error);
+
+	expect(result.errors[1]).toBeInstanceOf(HookError);
+	expect(result.errors[1].owner).toBe("qux");
+	expect(result.errors[1].cause).toBe(undefined);
+	expect(result.errors[1].rawCause).toBe("bar");
+});
+
 it("allows inspection of owner registered hooks", () => {
 	const system = new HookSystem();
 
@@ -123,4 +205,9 @@ it("allows inspection of owner registered hooks", () => {
 			.hooksForOwner("bar")
 			.map((registeredHook) => registeredHook.meta.name),
 	).toStrictEqual(["hook1"]);
+	expect(
+		system
+			.hooksForOwner("baz")
+			.map((registeredHook) => registeredHook.meta.name),
+	).toStrictEqual([]);
 });
